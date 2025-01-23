8cb5ccfa145e013af63cdeaaa9762d1dec30f75c
Alan <alanrd772@gmail.com>
2025-01-23 13:19:18 +0400

    Add links to big-brain-content root page

--- src/routes/big-brain-content/+page.svelte
+++ src/routes/big-brain-content/+page.svelte
@@ -1,1 +1,15 @@
+<script lang="ts">
+	import data from '$lib/data';
+	import TextAnimatedDecoration from '$lib/components/TextAnimatedDecoration.svelte';
+</script>
+
 <h1 class="text-4xl">big brain content.</h1>
+<ul class="text-lg">
+    {#each data['big-brain-content'] as content}
+        <li>
+            <TextAnimatedDecoration>
+                <a class="text-gray-500 transition hover:text-gray-500/75" href="/big-brain-content/{content.slug}/">{content.title}</a>
+            </TextAnimatedDecoration>
+        </li>
+    {/each}
+</ul>
